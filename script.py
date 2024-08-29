b340613ec542e3e3810fc73e02151238ae0094e0
Joel <Joel@DESKTOP-E4SG2VV>
2024-08-29 11:33:31 +1000

    Updated script

--- script.py
+++ script.py
@@ -1,25 +1,39 @@
 import mss
+from PIL import Image, ImageGrab
+import pyautogui
+
+import cv2 as cv
 import numpy as np
-import cv2
 import time
-import keyboard
+
 import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='last.pt', force_reload=True)
+model.conf = 0.005
 
-with mss.mss() as sct:
-	monitor = {"top": 0, "left": 0, "width": 2560, "height": 1440}
-	
+w, h = pyautogui.size()
+print("PIL Screen Capture Speed Test")
+print("Screen Resolution: " + str(w) + 'x' + str(h))
+
+img = None
+t0 = time.time()
+n_frames = 1
+
 while True:
-	t = time.time()
-	img = np.array(sct.grab(monitor))
-	results = model(img)
-	
-	cv2.imshow("s", np.squeeze(results.render()))
+    img = ImageGrab.grab()
+    img = np.array(img)                         # Convert to NumPy array
+    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)    # Convert RGB to BGR color
+    
+    small = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
+    results = model(small)
+    cv.imshow("Computer Vision", np.squeeze(results.render()))
 
-	print("FPS: {}".format(1 / (time.time - t)))
-
-	if keyboard.is_pressed("q"):
-		break
-
-cv2.destroyAllWindows()
+    # Break loop and end test
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        break
+    
+    elapsed_time = time.time() - t0
+    avg_fps = (n_frames / elapsed_time)
+    print("Average FPS: " + str(avg_fps))
+    n_frames += 1
