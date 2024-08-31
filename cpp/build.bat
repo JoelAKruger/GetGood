edb3ff593c071de06953b9ff55f2c6fef1cc9d8d
JoelTheDecent04 <jkru0007@student.monash.edu>
2024-09-01 06:01:33 +1000

    c++ image recognition

--- cpp/build.bat
+++ cpp/build.bat
@@ -1,2 +1,3 @@
-clang -I..\..\opencv\build\include main.cpp -o ValorantModel.exe
+clang++ -std=c++20 -I..\..\opencv\build\include main.cpp -o ValorantModel.exe
+rem cl -EHsc main.cpp -I..\..\opencv\build\include -Iinclude
 del *.obj
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,47 +1,96 @@
 #include <opencv2/opencv.hpp>
 #include <Windows.h>
+#include <onnxruntime_cxx_api.h>
 
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "onnxruntime.lib")
 #pragma comment(lib, "../../opencv/build/x64/vc16/lib/opencv_world4100.lib")
 
-int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE, LPWSTR CommandLine, int ShowCode)
+struct game_context
 {
-    HWND ValWindow = FindWindowEx(0, 0, "Notepad", 0);
+    HWND Window;
+    int Width, Height;
+    HDC DC;
+    HDC CompatibleDC;
+    HBITMAP Bitmap;
+    BITMAPINFOHEADER BitmapHeader;
     
+    void* Pixels;
+};
+
+#define INPUT_DIMENSION 640
+
+game_context CreateGameContext()
+{
+    game_context Context = {};
+    
+    Context.Window = GetForegroundWindow();
+    /*
     if (!ValWindow)
     {
         MessageBoxA(0, "Could not find Valorant window", "Error", MB_OK|MB_ICONWARNING);
         return 0;
     }
+*/
     
-    HDC ValWindowDC = GetDC(ValWindow);
+    Context.DC = GetDC(Context.Window);
     
     RECT WindowRect = {};
-    GetClientRect(ValWindow, &WindowRect);
+    GetClientRect(Context.Window, &WindowRect);
     
-    int ValWidth = WindowRect.right;
-    int ValHeight = WindowRect.bottom;
+    Context.Width = WindowRect.right;
+    Context.Height = WindowRect.bottom;
     
-    HDC ValWindowCompatibleDC = CreateCompatibleDC(ValWindowDC);
-    HBITMAP ValWindowBitmap = CreateCompatibleBitmap(ValWindowDC, ValWidth, ValHeight);
+    Context.CompatibleDC = CreateCompatibleDC(Context.DC);
+    Context.Bitmap = CreateCompatibleBitmap(Context.DC, INPUT_DIMENSION, INPUT_DIMENSION);
     
-    SelectObject(ValWindowCompatibleDC, ValWindowBitmap);
-    BitBlt(ValWindowCompatibleDC, 0, 0, ValWidth, ValHeight, ValWindowDC, 0, 0, SRCCOPY);
+    SelectObject(Context.CompatibleDC, Context.Bitmap);
+    StretchBlt(Context.CompatibleDC, 0, 0, INPUT_DIMENSION, INPUT_DIMENSION, Context.DC, 0, 0, Context.Width, Context.Height, SRCCOPY);
     
-    BITMAPINFOHEADER BitmapHeader = {};
-    BitmapHeader.biSize = sizeof(BITMAPINFOHEADER);
-    BitmapHeader.biWidth = ValWidth;
-    BitmapHeader.biHeight = -ValHeight;
-    BitmapHeader.biPlanes = 1;
-    BitmapHeader.biBitCount = 32;
-    BitmapHeader.biCompression = BI_RGB;
+    Context.BitmapHeader.biSize = sizeof(BITMAPINFOHEADER);
+    Context.BitmapHeader.biWidth = INPUT_DIMENSION;
+    Context.BitmapHeader.biHeight = -INPUT_DIMENSION;
+    Context.BitmapHeader.biPlanes = 1;
+    Context.BitmapHeader.biBitCount = 32;
+    Context.BitmapHeader.biCompression = BI_RGB;
     
-    void* Pixels = VirtualAlloc(0, ValWidth * ValHeight * 4, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+    Context.Pixels = VirtualAlloc(0, INPUT_DIMENSION * INPUT_DIMENSION * 4, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
     
-    GetDIBits(ValWindowCompatibleDC, ValWindowBitmap, 0, ValHeight, Pixels, (BITMAPINFO*)&BitmapHeader, DIB_RGB_COLORS);
+    GetDIBits(Context.CompatibleDC, Context.Bitmap, 0, INPUT_DIMENSION, Context.Pixels,  (BITMAPINFO*)&Context.BitmapHeader, DIB_RGB_COLORS);
     
-    cv::Mat Image(ValHeight, ValWidth, CV_8UC4, Pixels);
+    return Context;
+}
+
+int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE, LPWSTR CommandLine, int ShowCode)
+{
+    wchar_t* ModelPath = L"../volov8s_32batch_300epoch.onnx";
+    
+    auto providers = Ort::GetAvailableProviders();
+    for (auto provider : providers) {
+        OutputDebugStringA(provider.c_str());
+        OutputDebugStringA("\n");
+    }
+    
+    Sleep(3000);
+    game_context Game = CreateGameContext();
+    
+    cv::Mat Image(INPUT_DIMENSION, INPUT_DIMENSION, CV_8UC4, Game.Pixels);
+    
+    Ort::Env env = Ort::Env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING, "Default");
+    Ort::SessionOptions sessionOptions;
+    sessionOptions.SetInterOpNumThreads(1);
+    sessionOptions.SetIntraOpNumThreads(1);
+    sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_DISABLE_ALL);
+    
+    Ort::Session session = Ort::Session(env, ModelPath, sessionOptions);
+    
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+    
+    //std::vector<Ort::Value> inputTensor;        // Onnxruntime allowed input
+    
+    ///Ort::Value::CreateTensor<float>(memory_info, (float*)Image.data, input_tensor_size, input_node_dims[0].data(), input_node_dims[0].size());
+    
     cv::imshow("Valorant Window", Image);
     cv::waitKey(0);
     
